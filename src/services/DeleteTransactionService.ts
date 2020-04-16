4f95204334bca23e7774b0de0fab87b0da334d23
Leandro Souza <leandror.dev@gmail.com>
2020-04-16 17:12:50 -0300

    Delete Transaction feature

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,24 @@
-// import AppError from '../errors/AppError';
+import { isUuid } from 'uuidv4';
+import { getRepository } from 'typeorm';
+import Transaction from '../models/Transaction';
+
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getRepository(Transaction);
+
+    if (!isUuid(id)) {
+      throw new AppError('Invalid ID', 400);
+    }
+
+    const transaction = await transactionRepository.find({ where: { id } });
+
+    if (!transaction) {
+      throw new AppError('Invalid ID', 400);
+    }
+
+    await transactionRepository.delete(id);
   }
 }
 
