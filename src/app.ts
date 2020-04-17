6b197f0e5419b4e026d0558d58443a4b959799bd
Leandro Souza <leandror.dev@gmail.com>
2020-04-17 14:31:19 -0300

    Log in error

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  console.log(err);
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
       status: 'error',
