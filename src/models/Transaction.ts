16e5233df703a05f5d738ae1174ce6e319460873
Leandro Souza <leandror.dev@gmail.com>
2020-04-22 12:24:16 -0300

    Add eager: true in transactions model

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -27,7 +27,7 @@ class Transaction {
   @Column()
   category_id: string;
 
-  @ManyToOne(() => Category)
+  @ManyToOne(() => Category, { eager: true })
   @JoinColumn({ name: 'category_id' })
   category: Category;
 
